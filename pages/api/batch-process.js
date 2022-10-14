b2a4176ba5748f5da03b78db1546aadfc53fe2a3
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-14 11:33:34 +0100

    Add comment

--- pages/api/batch-process.js
+++ pages/api/batch-process.js
@@ -61,6 +61,7 @@ export default async function handler(req, res) {
   }
 }
 
+// This enables the function to run in the background for up to 15 minutes
 export const config = {
   type: "experimental-background",
 };
